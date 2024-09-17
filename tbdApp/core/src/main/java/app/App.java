72acc3a2ab845a451039645549edeb9764bc9722
August Solli Middelkoop <august.kampen@gmail.com>
2024-09-17 09:33:08 +0200

    Adding updated save to file and updated app.

--- tbdApp/core/src/main/java/app/App.java
+++ tbdApp/core/src/main/java/app/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,20 +13,14 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        // Load the FXML file from /resources/app/
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/app/App.fxml"));
-        AnchorPane root = loader.load();
-
-        // Create the scene with the FXML layout
-        Scene scene = new Scene(root, 400, 300);
-
-        // Set up the stage
-        primaryStage.setTitle("Group 52 - 2024");
+        GridPane root = loader.load();
+        Scene scene = new Scene(root, 400, 450);
+        primaryStage.setTitle("PixelArt");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    public static void main(String[] args) {
-        launch(args);
+    public static void main(String[] args) {launch(args);
     }
 }
