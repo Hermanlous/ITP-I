57413db84bb6c8270381a6599274c1a8ea650d9e
Johannes Hansen Aas <johhaa@stud.ntnu.no>
2024-10-10 12:37:03 +0200

    Feature: Implemented spotbugs, checkkstyle.
    Implemented spotbugs, and test coverage for AppcontrollerTest.
    Appcontroller test now clicks on pixelart creating art and deleting art.
    
    Co-authored by August, augustm@stud.ntnu.no

--- pixelart/ui/src/main/java/pixelart/ui/App.java
+++ pixelart/ui/src/main/java/pixelart/ui/App.java
@@ -3,28 +3,30 @@ package pixelart.ui;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image; // To change taskbar logo of the app
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 /**
- * The App class is the starter for the JavaFX application tdbApp
+ * The App class is the starter for the JavaFX application Pixelart
  * It sets up the FXML layout, sets up the scene, and displays the pixelart.
  * The application is for making pixel art.
  */
 public class App extends Application {
-
+    
     @Override
     public void start(Stage primaryStage) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/pixelart/ui/App.fxml"));
         GridPane root = loader.load();
         Scene scene = new Scene(root, 400, 450);
+
         primaryStage.setTitle("PixelArt");
         primaryStage.setScene(scene);
         primaryStage.show();
-    }
-
-    public static void main(String[] args) {launch(args);
+        Image taskbarLogo = new Image(getClass().getResourceAsStream("/pixelart/ui/taskbarLogo.png"));  // To change taskbar logo
+        primaryStage.getIcons().add(taskbarLogo);
+        
     }
 }
