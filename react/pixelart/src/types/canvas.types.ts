831d3993951d2da547a3a6390e3f44bbebcef80b
August Solli Middelkoop <august.kampen@gmail.com>
2024-11-12 15:03:14 +0100

    Documentation: JSdocs for canvas.types.ts and Canvas.tsx
    Implemented JSdocs for canvas.types.ts and Canvas.tsx. I will start on App.tsx now

--- react/pixelart/src/types/canvas.types.ts
+++ react/pixelart/src/types/canvas.types.ts
@@ -1,3 +1,16 @@
+
+/**
+ * Canvas props, used in Canvas.tsx.
+ * @param {CanvasProps}, props for the Canvas component.
+ * @param {string[][]}, is a 2-dimensional array the initial data.
+ * @param {string}, is the hexcode of the selected color, for drawing.
+ * @param {(x: number, y: number, color: string) => void}, function for resolving mouse events.
+ * @param {number}, defines max width of the Canvas component.
+ * @param {number}, defines max height of the Canvas component.
+ * @param {boolean}, either displays grid or does not display grid.
+ * @param {number}, establishes grid gap.
+ **/
+
 export interface CanvasProps {
     initialData: string[][];
     selectedColor: string;
