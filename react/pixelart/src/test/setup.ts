c431534322821c1ff15a34788618940b828b1cec
August Solli Middelkoop <august.kampen@gmail.com>
2024-11-12 21:39:14 +0100

    Documentation: JSdocs Everything
    Implemented Javadocs, for all components, hooks, api, tests, types, interfaces and pages.

--- react/pixelart/src/test/setup.ts
+++ react/pixelart/src/test/setup.ts
@@ -1,7 +1,10 @@
 import '@testing-library/jest-dom'
 import {cleanup} from '@testing-library/react';
 import {afterEach} from 'vitest';
-
+/**
+ * Runs after each test to clean up the DOM.
+ * This ensures that no leftover elements from previous tests remain in the DOM.
+ */
 afterEach(()=>{
   cleanup()
 })
